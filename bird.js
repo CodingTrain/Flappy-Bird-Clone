8d9faaa2fe1ed04711e343c28965bfd1f76e8cb8
Maya <mayaf2003@gmail.com>
2018-03-09 11:30:24 +0200

    Changed bird, pipe classes to ES6.

--- bird.js
+++ bird.js
@@ -3,24 +3,26 @@
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/cXgA1d_E-jY&
 
-function Bird() {
-  this.y = height / 2;
-  this.x = 64;
+class Bird {
+  constructor() {
+    this.y = height / 2;
+    this.x = 64;
 
-  this.gravity = 0.6;
-  this.lift = -15;
-  this.velocity = 0;
+    this.gravity = 0.6;
+    this.lift = -15;
+    this.velocity = 0;
+  }
 
-  this.show = function() {
+  show() {
     fill(255);
     ellipse(this.x, this.y, 32, 32);
   }
 
-  this.up = function() {
+  up() {
     this.velocity += this.lift;
   }
 
-  this.update = function() {
+  update() {
     this.velocity += this.gravity;
     this.velocity *= 0.9;
     this.y += this.velocity;
@@ -34,7 +36,5 @@ function Bird() {
       this.y = 0;
       this.velocity = 0;
     }
-
   }
-
 }
