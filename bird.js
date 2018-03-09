8d9cbc27d67c714cf0d311a0bab696d60eb232bd
musabkilic <musav70@gmail.com>
2018-03-09 19:39:22 +0300

    Added this.size to bird.js and improved the code for keeping the bird in the screen

--- bird.js
+++ bird.js
@@ -10,11 +10,12 @@ class Bird {
     this.gravity = 0.6;
     this.lift = -15;
     this.velocity = 0;
+    this.size = 32;
   }
 
   show() {
     fill(255);
-    ellipse(this.x, this.y, 32, 32);
+    ellipse(this.x, this.y, this.size, this.size);
   }
   
   up() {
@@ -26,13 +27,13 @@ class Bird {
     this.velocity *= 0.9;
     this.y += this.velocity;
     
-    if (this.y >= height) {
-      this.y = height;
+    if (this.y >= height - this.size/2) {
+      this.y = height - this.size/2;
       this.velocity = 0;
     }
 
-    if (this.y < 0) {
-      this.y = 0;
+    if (this.y <= this.size/2) {
+      this.y = this.size/2;
       this.velocity = 0;
     }
   }
