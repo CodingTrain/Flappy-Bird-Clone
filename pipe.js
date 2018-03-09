6c051d32e6e47c5c23a9f5bab1c5a0c962de2eff
Thomas Hammer <sparklez@detolly.no>
2018-03-09 18:40:15 +0100

    Made spacing consistent and made if fit the character

--- pipe.js
+++ pipe.js
@@ -4,24 +4,22 @@
 // Code for: https://youtu.be/cXgA1d_E-jY
 
 class Pipe {
-  constructor() {
-    this.spacing = 110;
-    this.top = random(height/6, 3/4*height);
+  constructor(bird) {
+    this.bird = bird;
+    this.spacing = this.bird.size*2.5;
+    this.top = random(height - this.spacing-16)+16;
     this.bottom = this.top + this.spacing;
 
     this.x = width;
     this.w = 20;
     this.speed = 2;
-
-    this.passed=false;
     this.highlight = false;
   }
 
-  hits(bird) {
-    if (bird.y - (bird.height / 2) < this.top || bird.y+(bird.height / 2) > this.bottom) {
-      if (bird.x +(bird.width / 2)> this.x + this.w && bird.x - (bird.width / 2) < this.x) {
+  hits() {
+    if (this.bird.y-this.bird.size/2 < this.top || this.bird.y+this.bird.size/2 > this.bottom) {
+      if (this.bird.x > this.x && this.bird.x < this.x + this.w) {
         this.highlight = true;
-        this.passed=true;
         return true;
       }
     }
@@ -29,18 +27,13 @@ class Pipe {
     return false;
   }
 
-  //this function is used to calculate scores and checks if we've went through the pipes
-  pass(bird) {
-        if (bird.x > this.x && !this.passed) {
-            this.passed=true;
-            return true;
-        }
-        return false;
-  }
-    
   show() {
-    image(pipeBodySprite,this.x,0,this.w,this.top);
-    image(pipeBodySprite,this.x,this.bottom,this.w,height);
+    fill(255);
+    if (this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, this.bottom, this.w, height - this.bottom);
   }
 
   update() {
--- sketch.js
+++ sketch.js
@@ -1,35 +1,22 @@
 // Daniel Shiffman
 // http://codingtra.in
 // http://patreon.com/codingtrain
-// Code for: https://youtu.be/cXgA1d_E-jY
+// Code for: https://youtu.be/cXgA1d_E-jY&
+
 
 var bird;
 var pipes = [];
-var score=0;
-var maxScore=0;
-
-var  pipeSprite,pipBodySprite;
-
-function preload() {
-
-pipeBodySprite = loadImage("./graphics/pipe_body.png");
-  
-}
-
 var bgImg;
 var bgX = 0;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 600);
   bird = new Bird();
-  pipes.push(new Pipe());
+  pipes.push(new Pipe(bird));
   bgImg = loadImage("graphics/background.png");
 }
 
 function draw() {
-
-  background(0);
-    
   // Draw our background image, then move it at the same speed as the pipes
   image(bgImg, bgX, 0, bgImg.width, height);
   bgX -= pipes[0].speed;
@@ -50,13 +37,8 @@ function draw() {
     pipes[i].show();
     pipes[i].update();
 
-    if (pipes[i].pass(bird)) {
-      console.log("YAY");
-      score++;
-    }
-      
     if (pipes[i].hits(bird)) {
-      gameover();
+      console.log("HIT");
     }
 
 
@@ -71,26 +53,13 @@ function draw() {
   bird.show();
 
   if (frameCount % 100 == 0) {
-    pipes.push(new Pipe());
+    pipes.push(new Pipe(bird));
   }
 
 
-  showScores();
+
 }
 
-function showScores() {
-    textSize(32);
-    text("score: "+score,1,32 );
-    text("record: "+maxScore,1,64 );   
-}
-
-function gameover() {
-      console.log("HIT");
-      textSize(64);  
-      text('HIT',width/2,height/2);
-      maxScore=max(score,maxScore);
-      score=0;
-}
 function keyPressed() {
   if (key == ' ') {
     bird.up();
