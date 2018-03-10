aaf38bea2fbd7affb7c9e9fb5f0f999b22854569
DaGuT <mangustalex11@gmail.com>
2018-03-10 12:27:25 +0700

    marshmallow pipe is funnier

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ var bgX = 0;
 
 function preload() {
   pipeBodySprite = loadImage("./graphics/horn_body_filled.png");
-  pipePeakSprite = loadImage("./graphics/horn_tip_filled.png");
+  pipePeakSprite = loadImage("./graphics/horn_body_filled.png");
   birdSprite = loadImage("graphics/train.png");
   bgImg = loadImage("graphics/background.png");
 }
